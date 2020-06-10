61ff86b9454bd85c57ffa1e6c800872a5de4554e
veed28 <onungwed@gmail.com>
2020-06-10 15:28:23 +0100

    created default user

--- src/EstateApp.Web/Startup.cs
+++ src/EstateApp.Web/Startup.cs
@@ -2,71 +2,122 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using EstateApp.Data.DatabaseContext.AuthenticationDbContext;
 using EstateApp.Data.DatabaseContext.ApplicationDbContext;
+using EstateApp.Data.DatabaseContext.AuthenticationDbContext;
+using EstateApp.Data.Entities;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace EstateApp.Web
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace EstateApp.Web {
+    public class Startup {
+        public Startup (IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDbContextPool<AuthenticationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("AuthenticationConnection"),
-               sqlServerOptions =>
-               {
-                   sqlServerOptions.MigrationsAssembly("EstateApp.Data");
-               }
-               ));
-            services.AddDbContextPool<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("ApplicationConnection"),
-               sqlServerOptions =>
-               {
-                   sqlServerOptions.MigrationsAssembly("EstateApp.Data");
-               }
-           ));
-            services.AddControllersWithViews();
+        public void ConfigureServices (IServiceCollection services) {
+            services.AddDbContextPool<AuthenticationDbContext> (options => options.UseSqlServer (Configuration.GetConnectionString ("AuthenticationConnection"),
+                sqlServerOptions => {
+                    sqlServerOptions.MigrationsAssembly ("EstateApp.Data");
+                }
+            ));
+            services.AddDbContextPool<ApplicationDbContext> (options => options.UseSqlServer (Configuration.GetConnectionString ("ApplicationConnection"),
+                sqlServerOptions => {
+                    sqlServerOptions.MigrationsAssembly ("EstateApp.Data");
+                }
+            ));
+
+            services.AddIdentity<ApplicationUser, IdentityRole> ()
+                .AddEntityFrameworkStores<AuthenticationDbContext> ()
+                .AddDefaultTokenProviders ();
+
+            services.Configure<IdentityOptions> (options => {
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 6;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.SignIn.RequireConfirmedEmail = false;
+                options.SignIn.RequireConfirmedPhoneNumber = false;
+            });
+
+            services.AddControllersWithViews ();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
+        public void Configure (IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider svp) {
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
+            } else {
+                app.UseExceptionHandler ("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                app.UseHsts ();
             }
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
+            app.UseHttpsRedirection ();
+            app.UseStaticFiles ();
 
-            app.UseRouting();
+            app.UseRouting ();
 
-            app.UseAuthorization();
+            app.UseAuthorization ();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
+            app.UseEndpoints (endpoints => {
+                endpoints.MapControllerRoute (
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            MigrateDatabaseContexts (svp);
+            CreateDefaultRolesAndUsers (svp).GetAwaiter ().GetResult ();
+
+        }
+
+        public void MigrateDatabaseContexts (IServiceProvider svp) {
+            var authenticationDbContext = svp.GetRequiredService<AuthenticationDbContext> ();
+            authenticationDbContext.Database.Migrate ();
+
+            var applicationDbContext = svp.GetRequiredService<ApplicationDbContext> ();
+            applicationDbContext.Database.Migrate ();
+        }
+
+        public async Task CreateDefaultRolesAndUsers (IServiceProvider svp) {
+            string[] roles = new string[] { "SystemAdministrator", "Agent", "User" };
+            var userEmail = "admin@estateapp.com";
+            var userPassword = "secretpassword";
+
+            var roleManager = svp.GetRequiredService<RoleManager<IdentityRole>> ();
+            foreach (var role in roles) {
+                var roleExist = await roleManager.RoleExistsAsync (role);
+                if (!roleExist) {
+                    await roleManager.CreateAsync (new IdentityRole { Name = role });
+                };
+            }
+
+            var userManager = svp.GetRequiredService<UserManager<ApplicationUser>> ();
+            var user = await userManager.FindByEmailAsync (userEmail);
+            if (user is null) {
+                user = new ApplicationUser {
+                    Email = userEmail,
+                    UserName = userEmail,
+                    EmailConfirmed = true,
+                    PhoneNumber = "07037494072",
+                    PhoneNumberConfirmed = true
+
+                };
+
+                await userManager.CreateAsync (user, userPassword);
+
+                await userManager.AddToRolesAsync (user, roles);
+
+            }
+
         }
     }
 }
